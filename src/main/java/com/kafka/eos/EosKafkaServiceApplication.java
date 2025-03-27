673285546894a1939ad48e8efff32419b3afdf8e
srikar-rao <67662872+srikar-rao@users.noreply.github.com>
2025-03-26 21:17:24 -0400

    master commit

---
+++ src/main/java/com/kafka/eos/EosKafkaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.eos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EosKafkaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EosKafkaServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kafka/eos/EosKafkaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.eos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EosKafkaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
